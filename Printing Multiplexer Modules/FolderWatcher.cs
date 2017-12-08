aa828e9c665d25eb0a835c1a97e99c115c7e05e1
Dylan Laufenberg <edev455@gmail.com>
2017-12-07 17:33:39 -0800

    Tune FolderWatcher to (hopefully) Give to the next module only when a file is actually ready. Not yet tested; will test this by connecting a next module that immediately tries to open the file.

--- Printing Multiplexer Modules/FolderWatcher.cs
+++ Printing Multiplexer Modules/FolderWatcher.cs
@@ -10,17 +10,20 @@ namespace Printing_Multiplexer
 {
     class FolderWatcher : BasicModule
     {
+        const string NextModule = "NextModule";
+
         FileSystemWatcher fsw = new FileSystemWatcher();
 
         public FolderWatcher()
         {
-            Outputs = new OutputCollection("NextModule");
+            Outputs = new OutputCollection(NextModule);
 
-            // Setup the FileSystemWatcher, aside from the Path.
+            // Now, Set up the FileSystemWatcher, aside from the Path.
+
             fsw.IncludeSubdirectories = false;
-            fsw.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+            // Use the LastWrite filter so that we can (in theory) check once per file write and not have to loop to wait for the file to become available. That SHOULD mean that, in OnChanged, we simply check whether the file is available to open, yet, and if not, we wait for another event.
+            fsw.NotifyFilter = NotifyFilters.LastWrite;
             fsw.Changed += new FileSystemEventHandler(OnChanged);
-            fsw.Renamed += new RenamedEventHandler(OnRenamed);
         }
 
         public override void Give(string path)
@@ -50,16 +53,63 @@ namespace Printing_Multiplexer
             return true;
         }
 
-        // TODO Do something when files are changed, etc. and verify that all desired changes actually notify.
-        // TODO Make the notifications customizable?
-        private static void OnChanged(Object source, FileSystemEventArgs e)
+        // When a file is created, wait for it to be ready, then give it to NextModule.
+        private void OnChanged(Object source, FileSystemEventArgs e)
         {
-            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            // We use Changed instead of Created because Changed events will fire as the file is written; Created is only fired once, at the start, and we need to detect the end of the file copy.
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                // Wrong event type. Ignore.
+                return;
+            }
+            FileInfo f = null;
+
+            try
+            {
+                f = new FileInfo(e.FullPath);
+            }
+            catch(Exception exception)
+            {
+                // Well, guess we won't be handling this file.... Ignore.
+                return;
+            }
+
+            if(!isFileUnlocked(f))
+            {
+                // File's not ready yet. Ignore.
+                return;
+            }
+
+            // File is READY!
+            Outputs.GetOutput(NextModule)?.Give(e.FullPath);
         }
 
-        private static void OnRenamed(object source, RenamedEventArgs e)
+        // Copied and lightly modified from:
+        // https://stackoverflow.com/questions/876473/is-there-a-way-to-check-if-a-file-is-in-use/937558#937558
+        private bool isFileUnlocked(FileInfo file)
         {
-            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                // The file is unavailable because it is:
+                // still being written to
+                // or being processed by another thread
+                // or does not exist (has already been processed).
+                return false;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            // File is not locked.
+            return true;
         }
     }
 }
