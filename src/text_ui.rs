b4fd0f851ce2c94960e3ca79395699d26c11b353
Dylan Laufenberg <edev455@gmail.com>
2022-12-19 01:06:25 -0800

    Add a text UI input loop.
    Currently it simply waits for the user to enter a line and then quits.

--- src/text_ui.rs
+++ src/text_ui.rs
@@ -1,4 +1,7 @@
 use crate::controller::{ChannelPair, ControlMessage, StatusMessage, UIControlMessage};
+use crossbeam::channel;
+use std::io;
+use std::thread;
 
 // A simple, text-based UI for controlling the printing operation.
 //
@@ -14,23 +17,76 @@ impl TextUI {
     }
 
     pub fn run(&self) {
-        self.controller
-            .sender
-            .send(UIControlMessage::Status(StatusMessage::Notice(
-                "TextUI just popping by.".to_string(),
-            )))
-            .unwrap();
-        self.controller.sender.send(UIControlMessage::Exit).unwrap();
+        // Start reading stdin in a new thread.
+        let stdin = self.read_stdin();
 
-        match self.controller.receiver.recv().unwrap() {
-            ControlMessage::Close => {
-                self.controller
-                    .sender
-                    .send(UIControlMessage::Status(StatusMessage::Notice(
-                        "TextUI gracefully closing".to_string(),
-                    )))
-                    .unwrap();
+        loop {
+            // Set up a crossbeam select operation among our various receivers.
+            let mut select = channel::Select::new();
+            let stdin_index = select.recv(&stdin);
+            let controller_index = select.recv(&self.controller.receiver);
+            let operation = select.select();
+            match operation.index() {
+                // Receive a line from standard input.
+                i if i == stdin_index => {
+                    match operation.recv(&stdin) {
+                        Ok(line) => {
+                            // Process the line that the user entered.
+                            break
+                        },
+                        Err(_) => {
+                            eprintln!("TextUI's stdin channel disconnected unexpectedly");
+                            break
+                        }
+                    }
+                },
+
+                // Receive a message from the controller.
+                i if i == controller_index => {
+                    match operation.recv(&self.controller.receiver) {
+                        Ok(message) => match message {
+                            ControlMessage::Close => {
+                                self.controller
+                                    .sender
+                                    .send(UIControlMessage::Status(StatusMessage::Notice(
+                                        "TextUI gracefully closing".to_string(),
+                                    )))
+                                    .unwrap();
+                                break
+                            }
+                        },
+
+                        Err(_) => {
+                            eprintln!("Controller's TextUI channel disconnected unexpectedly");
+                            break
+                        }
+                    }
+                },
+
+                // Uh oh. We somehow received an index that shouldn't have been possible. Bug!
+                i => panic!(
+                    "Controller::run() received an invalid operation index from select: {}",
+                    i
+                ),
             }
         }
+    }
+
+    // Spawns a new thread that reads from stdin and sends lines a crossbeam::channel::Receiver.
+    // This prevents blocking in the main TextUI thread and allows the main TextUI thread to select
+    // from this or any other ready Receiver.
+    fn read_stdin(&self) -> channel::Receiver<String> {
+        let (sender, receiver) = channel::unbounded();
+        thread::spawn(move || {
+            let stdin = io::stdin();
+            loop {
+                let mut buffer = String::new();
+                match stdin.read_line(&mut buffer) {
+                    Ok(_) => sender.send(buffer).expect("Stdin reader tried to send text, but the channel was disconnected!"),
+                    Err(e) => panic!("{:?}", e),
+                }
+            }
+        });
+        receiver
     }
 }
