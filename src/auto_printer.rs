5b7e4783529b3b548309017fc906c70c443d2b55
Dylan Laufenberg <edev455@gmail.com>
2022-12-19 15:09:28 -0800

    Add main run loop for AutoPrinter.

--- src/auto_printer.rs
+++ src/auto_printer.rs
@@ -2,6 +2,8 @@ use crate::controller::{ChannelPair, ControlMessage, StatusMessage};
 use crossbeam::channel;
 
 pub struct AutoPrinter {
+    // TODO Add a printer name, once we have one. Use it in messages.
+
     // The lines of communication to and from the controller.
     controller: ChannelPair<StatusMessage, ControlMessage>,
 
@@ -23,5 +25,56 @@ impl AutoPrinter {
     }
 
     pub fn run(&self) {
+        loop {
+            // Set up a crossbeam select operation among our various receivers.
+            let mut select = channel::Select::new();
+            let print_queue_index = select.recv(&self.print_queue);
+            let controller_index = select.recv(&self.controller.receiver);
+            let operation = select.select();
+            match operation.index() {
+                // Receive a line from standard input.
+                i if i == print_queue_index => match operation.recv(&self.print_queue) {
+                    Ok(path) => {
+                        self.controller
+                            .sender
+                            .send(StatusMessage::Notice(format!(
+                                "AutoPrinter picked up file to print: {}",
+                                path
+                            )))
+                            .unwrap();
+                        // TODO Actually print the thing.
+                    }
+                    Err(_) => {
+                        eprintln!("Print queue senders disconnected unexpectedly");
+                        break;
+                    }
+                },
+                // Receive a message from the controller.
+                i if i == controller_index => match operation.recv(&self.controller.receiver) {
+                    Ok(message) => match message {
+                        ControlMessage::Close => {
+                            self.controller
+                                .sender
+                                .send(StatusMessage::Notice(
+                                    "AutoPrinter gracefully closing".to_string(),
+                                ))
+                                .unwrap();
+                            break;
+                        }
+                    },
+
+                    Err(_) => {
+                        eprintln!("Controller's AutoPrinter channel disconnected unexpectedly");
+                        break;
+                    }
+                },
+
+                // Uh oh. We somehow received an index that shouldn't have been possible. Bug!
+                i => panic!(
+                    "AutoPrinter::run() received an invalid operation index from select: {}",
+                    i
+                ),
+            }
+        }
     }
 }
